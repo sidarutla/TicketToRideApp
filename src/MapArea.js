2cff97a9c7f700f492365703f8104ca9e8707d7a
Shashi Nallu <ssatechca@gmail.com>
2022-05-12 22:12:57 -0700

    Adds tracks to the map.

--- src/MapArea.js
+++ src/MapArea.js
@@ -1,22 +1,25 @@
 import React from 'react';
 
 
-import { Stage, Layer} from 'react-konva';
+import { Stage, Layer, Rect, Circle} from 'react-konva';
 
 import Grid from '@mui/material/Grid';
 
 function MapArea(props) {
 
-    // const {board} = props;
+    const {board} = props;
 
     return (
       <Grid container justifyContent="center" spacing={2}>
       <Stage width={886} height={600} style={{backgroundImage:"url(map.jpeg)", backgroundSize:"contain"}}>
       <Layer>
       {
-          // <Rect y={10} width={50} height={50} fill="red" />
-          // <Circle x={200} y={200} stroke="black" radius={50} />
-
+          <>
+          <Rect y={10} x={0} width={35} height={10} rotation={10} fill="red" />
+          <Rect y={16} x={38} width={35} height={10} rotation={30} fill="red" />
+          <Rect y={35} x={72} width={35} height={10} rotation={45} fill="red" />
+          <Circle x={200} y={200} stroke="black" radius={50} />
+          </>
       }
       </Layer>
     </Stage>
