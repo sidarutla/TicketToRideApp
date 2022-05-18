bb7a91011c7a4f7768bf4f472fed26a316a4bd02
Shashi Nallu <ssatechca@gmail.com>
2022-05-17 20:31:32 -0700

    Adds capability to mark cities.

--- src/MapArea.js
+++ src/MapArea.js
@@ -5,7 +5,18 @@ import { Stage, Layer, Rect, Circle} from 'react-konva';
 
 import Grid from '@mui/material/Grid';
 
-import {getConnection} from "./MapData";
+import {getConnection, getCity} from "./MapData";
+import {getPlayer} from './boardutil'
+
+
+function City(props) {
+    const {city, color} = props;
+    const x1 = city.point[0];
+    const y1 = city.point[1];
+    return (
+         <Circle x={x1} y={y1} stroke={color} fill={color} radius={8} />
+    )
+}
 
 
 function Track(props) {
@@ -50,37 +61,74 @@ function Pathway(props) {
 
 function MapArea(props) {
 
-    const drawingMode = false;
+    const trackMode = false;
+    const cityMode = true;
+
+    const {showTickets=true}  = props;
     const [points, setPoints] = useState([]);
     const [tracks, setTracks] = useState([]);
 
     const width = 900;
     const height = 600
-    const {board} = props;
+    const {board, playerID} = props;
+
+    const thePlayer = getPlayer(board, playerID);
+
+
+    //Group have tickets by pairs.. and give a number to the route...
+    //Group the tickets drawan same.. but give striped...or glowing background these pairs..
+    //Both of them should be shown only when showTickets is on...
+    //Combie with the show tickets option...
+    const citiesToShow = thePlayer.tickets.flatMap((ticket)=>{
+        return [ticket.source, ticket.destination];
+    })
+
+    // const citiesDrawan = thePlayer.drawnTickets.flatMap((ticket)=>{
+    //     return [ticket.source, ticket.destination];
+    // })
 
     let occupiedPaths = board.connections.flatMap(connection=>{
         const occupiedPaths = [];
-        if(connection.pathway1 && (connection.pathway1.gamePlayer != null || drawingMode)) {
+        if(connection.pathway1 && (connection.pathway1.gamePlayer != null || trackMode)) {
             const con = getConnection(connection.source, connection.destination);
             if(con) {
                 occupiedPaths.push({
                     tracks:con.pathway1,
-                    color:drawingMode ? "red" : connection.pathway1.gamePlayer.playerColor
+                    color:trackMode ? "red" : connection.pathway1.gamePlayer.playerColor
 
                 })
             }
         }
-        if(connection.pathway2 && (connection.pathway2.gamePlayer != null || drawingMode)) {
+        if(connection.pathway2 && (connection.pathway2.gamePlayer != null || trackMode)) {
             const con = getConnection(connection.source, connection.destination);
             if(con) {
                 occupiedPaths.push({
                     tracks:con.pathway2,
-                    color: drawingMode ? "blue" : connection.pathway2.gamePlayer.playerColor
+                    color: trackMode ? "blue" : connection.pathway2.gamePlayer.playerColor
                 })
             }
         }
         return occupiedPaths;
     })
+
+    let allCities = board.connections.reduce((uniqueList, connection)=>{
+        if(!uniqueList.includes(connection.source)){
+            uniqueList.push(connection.source);
+        }
+        if(!uniqueList.includes(connection.destination)){
+            uniqueList.push(connection.destination);
+        }
+        return uniqueList;
+    },[]);
+
+    const allCityJson = allCities.sort((c1, c2)=> c1.localeCompare(c2)).map((cityName)=>{
+        return {
+            name: cityName,
+            points:[]
+        }
+    })
+    // console.log("city json", JSON.stringify(allCityJson));
+
     occupiedPaths = occupiedPaths.filter(p=>p!=null)
     if(!occupiedPaths) {
         occupiedPaths = [];
@@ -88,30 +136,39 @@ function MapArea(props) {
 
     return (
       <Grid container justifyContent="center" spacing={2} onClick={(event)=>{
-              if(!drawingMode){
+              if(!trackMode && !cityMode){
                   return;
               }
-              if(event.detail === 2) {
-                  setTimeout(()=>{
-                      setPoints([]);
-                      setTracks([]);
-                  }, 300);
-                  return;
-              } else {
-                  let newPoints = [...points]
+
+              if(trackMode) {
+                  if(event.detail === 2) {
+                      setTimeout(()=>{
+                          setPoints([]);
+                          setTracks([]);
+                      }, 300);
+                      return;
+                  } else {
+                      let newPoints = [...points]
+                      newPoints.push(event.nativeEvent.layerX)
+                      newPoints.push(event.nativeEvent.layerY)
+                      if(newPoints.length >= 4) {
+                          const str = "[" + newPoints.join() +"]"
+
+                          let newTracks = [...tracks];
+                          newTracks.push(str);
+                          setTracks(newTracks);
+
+                          newPoints = [];
+                          console.log(newTracks.join());
+                      }
+                      setPoints(newPoints);
+                  }
+              } else if(cityMode){
+                  let newPoints = []
                   newPoints.push(event.nativeEvent.layerX)
                   newPoints.push(event.nativeEvent.layerY)
-                  if(newPoints.length >= 4) {
-                      const str = "[" + newPoints.join() +"]"
+                  console.log(newPoints.join());
 
-                      let newTracks = [...tracks];
-                      newTracks.push(str);
-                      setTracks(newTracks);
-
-                      newPoints = [];
-                      console.log(newTracks.join());
-                  }
-                  setPoints(newPoints);
               }
           }}>
       <Stage width={width} height={height} style={{backgroundImage:"url(board.png)", backgroundSize:"contain"}}>
@@ -124,6 +181,27 @@ function MapArea(props) {
                         <Pathway key={index} pathway={occupiedPath.tracks} color={occupiedPath.color}/>
                   )
               })
+          }
+          {
+              cityMode && allCities.map((city, index)=>{
+                  const theCity = getCity(city);
+                  if(theCity && theCity.point.length == 2) {
+                      return (
+                            <City key={index} city={theCity} color={thePlayer.playerColor}/>
+                      )
+                  } else {
+                      return null;
+                  }
+              })
+          }
+          {
+              // (
+              //     <>
+              //     <City city={getCity("Seattle")} color={"red"}/>
+              //     <City city={getCity("Vancouver")} color={"green"}/>
+              //     <City city={getCity("Portland")} color={"blue"}/>
+              //     </>
+              // )
           }
           </>
       }
