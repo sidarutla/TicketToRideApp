247d1b6238011b4b5f7e88d2e6e35b064ccb4f43
Shashi Nallu <ssatechca@gmail.com>
2022-05-21 02:01:18 -0700

    Moves the deployment to arutla.com

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,7 +1,7 @@
 export const APP_NAME="Ticket To Ride (Dev Mode)"
 export const ENVIRONMENT = process.env.REACT_APP_DEPLOY
-export const TTR_APP_URL = 'http://ttr.local.apheliontechnologies.com:4000'
-export const TTR_URL = 'http://ttrapi.local.apheliontechnologies.com:9080'
+export const TTR_APP_URL = 'http://ttr.local.arutla.com:4000'
+export const TTR_URL = 'http://ttrapi.local.arutla.com:9080'
 export const SOCKET_URL = TTR_URL + "/ttr-websocket";
 
 export const DEBUG=true
