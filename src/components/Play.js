d9fd3e5c38e0bd8496df0f97d640dc1b36a9fbf0
Shashi Nallu <ssatechca@gmail.com>
2022-06-10 18:37:43 -0700

    Displays the round number and shows final round message.

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,22 +1,30 @@
 import React from 'react';
 
+import { Typography } from '@mui/material';
+
 import PlayActions from './PlayActions';
 import PlayStatus from './PlayStatus';
 
 function Play(props) {
-    const {board, playerID} = props;
-    if(board.currentPlayerID === playerID) {
-        return (
+    const { board, playerID } = props;
+
+    return (
+        <div>
             <div>
-                It is your turn to play.
-                <PlayActions board={board} playerID={playerID}/>
+                Round : {board.round} {board.finalPlayerID && (<Typography variant="h6" style={{ color: "red" }}>(FINAL ROUND)</Typography>)}
             </div>
-        )
-    } else{
-        return (
-            <PlayStatus board={board}/>
-        )
-    }
+            {
+                (board.currentPlayerID === playerID) ? (
+                    <div>
+                        It is your turn to play.
+                        <PlayActions board={board} playerID={playerID} />
+                    </div>
+                ) : (
+                    <PlayStatus board={board} />
+                )
+            }
+        </div>
+    )
 }
 
 export default Play;
