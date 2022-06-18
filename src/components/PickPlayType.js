e87980199ea3f9fbdecd0aeb9d1eacc61bb274b0
Shashi Nallu <ssatechca@gmail.com>
2022-06-18 13:40:31 -0700

    Replaces browser's native confirm dialog with MUI dialog.

--- src/components/PickPlayType.js
+++ src/components/PickPlayType.js
@@ -1,21 +1,53 @@
+import React, { useState } from 'react';
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 
+import { pickPlay } from '../lib';
 
-import {pickPlay} from '../lib';
+const DrawTicketsConfirmDialog = (props) => {
+    return (
+        <Grid>
+            <Dialog
+                open={props.open}
+                onClose={props.onClose}
+            >
+                <DialogTitle id="alert-dialog-title">
+                </DialogTitle>
+
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Are you sure you want to draw tickets? You won't be able to change play type after your draw tickets.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="contained" onClick={props.onClose} autoFocus color="secondary">
+                        Cancel
+                    </Button>
+                    <Button variant="contained" onClick={props.onConfirm} color="primary">Confirm</Button>
+                </DialogActions>
+            </Dialog>
+        </Grid>
+    );
+}
 
 function PickPlayType(props) {
-    const {board, playerID} = props;
-    const {round} = board;
+
+    const [confirmDrawTicketsOpen, setConfirmDrawTicketsOpen] = useState(false);
+
+    const { board, playerID } = props;
+    const { round } = board;
 
     const handlePickPlayType = (playerID, boardID, playType) => {
-        if(playType === "drawTickets") {
-            if(round !== 1) {
-                let result = confirm('Are you sure you want to draw additional tickets?');
-                if(result) {
-                    pickPlay(playerID, boardID, playType);
-                }
+        if (playType === "drawTickets") {
+            if (round !== 1) {
+                setConfirmDrawTicketsOpen(true);
             } else {
                 pickPlay(playerID, boardID, playType);
             }
@@ -24,33 +56,43 @@ function PickPlayType(props) {
         }
     }
 
+    const handleConfirmDrawTicketsClose = () =>{
+        setConfirmDrawTicketsOpen(false);
+    }
+
+    const handleConfirmDrawTickets = () => {
+        setConfirmDrawTicketsOpen(false);
+        pickPlay(playerID, board.boardID, "drawTickets");
+    }
+
     return (
-        <Grid item container xs={12} justifyContent="center" style={{paddingTop:"10px"}}>
+        <Grid item container xs={12} justifyContent="center" style={{ paddingTop: "10px" }}>
             <Grid item xs={12}>
                 What would you like to do?
             </Grid>
 
             <Grid item xs={12} container justifyContent="center">
                 <Stack direction="row" spacing={2}>
-                <Button variant="contained" color="primary"
-                    onClick={()=>{handlePickPlayType(playerID, board.boardID, "drawTickets")}}>
-                    Draw Tickets
-                </Button>
+                    <Button variant="contained" color="primary"
+                        onClick={() => { handlePickPlayType(playerID, board.boardID, "drawTickets") }}>
+                        Draw Tickets
+                    </Button>
 
-                <Button variant="contained" color="primary"
-                    disabled={board.round === 1 ? true : false}
-                    onClick={()=>{handlePickPlayType(playerID, board.boardID, "drawCards")}}>
-                    Draw Cards
-                </Button>
+                    <Button variant="contained" color="primary"
+                        disabled={board.round === 1 ? true : false}
+                        onClick={() => { handlePickPlayType(playerID, board.boardID, "drawCards") }}>
+                        Draw Cards
+                    </Button>
 
-                <Button variant="contained" color="primary"
-                    disabled={board.round === 1 ? true : false}
-                    onClick={()=>{handlePickPlayType(playerID, board.boardID, "buildTracks")}}>
-                    Build Track
-                </Button>
-
+                    <Button variant="contained" color="primary"
+                        disabled={board.round === 1 ? true : false}
+                        onClick={() => { handlePickPlayType(playerID, board.boardID, "buildTracks") }}>
+                        Build Track
+                    </Button>
                 </Stack>
             </Grid>
+
+            <DrawTicketsConfirmDialog open={confirmDrawTicketsOpen} onClose={handleConfirmDrawTicketsClose} onConfirm={handleConfirmDrawTickets}/>
         </Grid>
     )
 }
--- src/components/ReturnTickets.js
+++ src/components/ReturnTickets.js
@@ -1,5 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
@@ -7,22 +12,86 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 
 
-import {getCurrentPlayer} from '../boardutil'
+import { getCurrentPlayer } from '../boardutil'
+import { returnTickets } from '../lib';
 
-import {returnTickets} from '../lib';
+
+const ReturnTicketsDialog = (props) => {
+    const { tickets } = props;
+
+
+    const returnTickets = tickets.filter(ticket => ticket.selected);
+
+    const renderReturnTickets = () => {
+        if (returnTickets.length === 0) {
+            return (
+                <DialogContentText id="alert-dialog-description">
+                    You are not returning any tickets. Are you sure you want to continue?
+                </DialogContentText>
+            )
+        } else {
+            return (
+                <div>
+                    <DialogContentText id="alert-dialog-description">
+                        You are returning the following tickets. Are you sure you want to continue?
+                    </DialogContentText>
+                    <ul>
+                        {
+                            returnTickets.map((ticket, index) => {
+                                return (
+                                    <li key={index}>{ticket.source + " - " + ticket.destination + " (" + ticket.value + ")"}</li>
+                                )
+                            })
+                        }
+                    </ul>
+
+                </div>
+            )
+        }
+    }
+
+    return (
+        <Grid>
+            <Dialog
+                open={props.open}
+                onClose={props.onClose}
+            >
+                <DialogTitle id="alert-dialog-title">
+                </DialogTitle>
+
+                <DialogContent>
+                    {renderReturnTickets()}
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="contained" onClick={props.onClose} autoFocus color="secondary">
+                        Cancel
+                    </Button>
+                    <Button variant="contained" onClick={props.onConfirm} color="primary">
+                        Continue
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </Grid>
+    );
+}
+
+
 
 function ReturnTickets(props) {
+
+    const [confirmReturnTicketsOpen, setConfirmReturnTicketsOpen] = useState(false);
+
     const [errorMessage, setErrorMessage] = useState(null);
-    const {board, playerID} = props;
+    const { board, playerID } = props;
     const currentPlayer = getCurrentPlayer(board);
     const [ticketList, setTicketList] = useState([])
 
-    useEffect(()=>{
-        const selectableTickets = currentPlayer.drawnTickets.map(ticket=>{
-            return {...ticket, selected:false}
+    useEffect(() => {
+        const selectableTickets = currentPlayer.drawnTickets.map(ticket => {
+            return { ...ticket, selected: false }
         })
         setTicketList(selectableTickets);
-    },[currentPlayer])
+    }, [currentPlayer])
 
     const handleTicketSelection = (i) => (event) => {
         const newTicketList = [...ticketList];
@@ -31,30 +100,33 @@ function ReturnTickets(props) {
     }
 
 
-    const handleReturnTickets = () =>{
+    const handleReturnTickets = () => {
+        setErrorMessage(null);
         const maxTickets = board.round === 1 ? 1 : 2;
-        const returnTicketIds = ticketList.filter(ticket=>ticket.selected).map(ticket=>ticket.ticketID);
+        const returnTickets = ticketList.filter(ticket => ticket.selected);
 
-        if(returnTicketIds.length > maxTickets) {
+        if (returnTickets.length > maxTickets) {
             setErrorMessage("You can return only " + maxTickets + " ticket(s)...")
         } else {
-            let message = "";
-            if(returnTicketIds.length === 0) {
-                message = "Are you sure you don't want to return any tickets?"
-            } else {
-                message = "Are you sure you  want to return " + returnTicketIds.length + " ticket(s)?"
-            }
-            let result = confirm(message);
-            if(result) {
-                returnTickets(playerID, board.boardID, returnTicketIds)
-            }
+            setConfirmReturnTicketsOpen(true);
         }
     }
+
+    const handleReturnTicketsClose = () => {
+        setConfirmReturnTicketsOpen(false);
+    }
+
+    const handleReturnTicketsConfirm = () => {
+        setConfirmReturnTicketsOpen(false);
+        const returnTicketIds = ticketList.filter(ticket => ticket.selected).map(ticket => ticket.ticketID);
+        returnTickets(playerID, board.boardID, returnTicketIds)
+    }
+
 
     return (
         <Grid container justifyContent={"center"}>
 
-            <Grid style={{color:"red"}}>
+            <Grid style={{ color: "red" }}>
                 {errorMessage}
             </Grid>
 
@@ -66,26 +138,26 @@ function ReturnTickets(props) {
 
             {
                 ticketList && ticketList.length > 0 && (
-                    <Grid item container xs={12} justifyContent="center" style={{paddingTop:"20px", paddingBottom:"20px"}}>
+                    <Grid item container xs={12} justifyContent="center" style={{ paddingTop: "20px", paddingBottom: "20px" }}>
                         {
-                            ticketList.map((ticket, index)=>{
+                            ticketList.map((ticket, index) => {
                                 const ticketLabel = ticket.source + " - " + ticket.destination + " (" + ticket.value + ")"
-                            return (
-                                <Grid item container key={index} justifyContent={"center"}>
-                                    <Grid item>                                 
-                                        <FormControlLabel 
-                                            control={
-                                                <Checkbox 
-                                                    name={"selectedTickets"}
-                                                    value={ticket.ticketID}
-                                                    onChange={ handleTicketSelection(index)}
-                                                />} 
+                                return (
+                                    <Grid item container key={index} justifyContent={"center"}>
+                                        <Grid item>
+                                            <FormControlLabel
+                                                control={
+                                                    <Checkbox
+                                                        name={"selectedTickets"}
+                                                        value={ticket.ticketID}
+                                                        onChange={handleTicketSelection(index)}
+                                                    />}
                                                 label={ticketLabel} labelPlacement="end" />
+                                        </Grid>
                                     </Grid>
-                                </Grid>
-                            )
-                        })
-                    }
+                                )
+                            })
+                        }
                     </Grid>
                 )
             }
@@ -93,12 +165,19 @@ function ReturnTickets(props) {
 
             <Grid item xs={12} container justifyContent="center">
                 <Stack direction="row" spacing={2}>
-                <Button variant="contained" color="primary"
-                    onClick={()=>{handleReturnTickets()}}>
-                    Return Tickets
-                </Button>
+                    <Button variant="contained" color="primary"
+                        onClick={() => { handleReturnTickets() }}>
+                        Return Tickets
+                    </Button>
                 </Stack>
             </Grid>
+
+
+            <ReturnTicketsDialog
+                tickets={ticketList}
+                open={confirmReturnTicketsOpen}
+                onClose={handleReturnTicketsClose}
+                onConfirm={handleReturnTicketsConfirm} />
         </Grid>
     )
 }
